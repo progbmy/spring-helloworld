683a6b7b7466177e10bcb6af5a4db9e88e761e83
MIhail <skuter63@mail.ru>
2021-11-28 23:28:39 +0400

    add bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+       HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+       boolean resultBean = bean == bean1;
+       System.out.println(resultBean);
+
+       Cat bean2 = (Cat) applicationContext.getBean("cat");
+       Cat bean3 = (Cat) applicationContext.getBean("cat");
+       boolean resultBean1= bean2 == bean3;
+       System.out.println(resultBean1);
+
+        //System.out.println(bean.getMessage());
     }
 }
