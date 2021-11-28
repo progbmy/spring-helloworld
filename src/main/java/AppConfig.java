0bc6334029f3d5de2b8dd9bc9dbb3e446d81917d
MIhail <skuter63@mail.ru>
2021-11-28 23:56:44 +0400

    Finish

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+
 public class AppConfig {
  
     @Bean(name="helloworld")
@@ -10,5 +11,13 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name= "cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setMessage("Hello Cat");
+        return cat;
     }
 }
